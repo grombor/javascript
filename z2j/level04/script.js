bd626dfc118d3f432e61fa58943f83c4ce570bfa
grombor <gromgaming@yahoo.com>
2023-05-29 21:48:41 +0200

    added ui elements

---
+++ z2j/level04/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// const HandleClick = (e, id) => {
+//   e.preventDefault();
+//   const imageId = id.replace('list-', '');
+//   const targetImg = document.getElementById(imageId);
+//   targetImg.scrollIntoView({ behavior: 'smooth' });
+// };
+
+// const listItems = Array.from(document.querySelectorAll('[class^="list-item"]'));
+// listItems.forEach(item => item.addEventListener('click', (e) => {
+//   HandleClick(e, item.id);
+// }));
