67413c6031c2164739b88ced444cc3b2058adcf1
grombor <gromgaming@yahoo.com>
2023-06-01 22:37:16 +0200

    added imgs fixed js

--- z2j/level04/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function handleScroll(event) {
-    const element = document.querySelector('.carousel');
-    element.scrollLeft += (event.deltaY)*0.5;
-    event.preventDefault();
-  }
