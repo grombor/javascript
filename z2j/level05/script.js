cf9f5731db2e76b197d92514855244dec4c562a8
grombor <gromgaming@yahoo.com>
2023-06-11 22:16:03 +0200

    updated

---
+++ z2j/level05/script.js
@@ -0,0 +1,13 @@
+"use strict;"
+
+
+/* TODO:
+- Dodaj walidacje do pól, które tego wymagają. 
+- Powinna się uruchamiać po zatwiedzeniu formularza przyciskiem Wyślij (lub enter 😉).
+- Walidacja powinna być prosta, po prostu sprawdź czy dane pola są uzupełnione.
+- Ważne aby sprawdzić wszystkie potrzebne pola.
+- Jeżeli jest przynajmniej jeden błąd, zablokuj wykonywanie kolejnych akcji (patrz pkt niżej).
+- Po zatwierdzeniu formularza wyświetl alert oraz zadbaj o to, żeby strona się nie przeładowywała po jego wysłaniu.
+- Zwróć też uwagę, że oprócz wyświetlenia wartości wyświetla się też zwrot grzecznościowy odpowiedni dla wybranej płci. Dobrze aby w Twoim formularzu też było pole o tym typie i miało wpływ na wynik
+- Pola formularza: Imię, Nazwisko, Płeć, Login, Hasło, Data urodzenia, Przycisk wysyłający
+*/
